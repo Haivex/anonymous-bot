5458d72a2c83110ac0bad444f76b391656307a04
Haivex <demozial@gmail.com>
2021-06-08 13:34:50 +0200

    format with prettier

--- src/send-anonymous-message.ts
+++ src/send-anonymous-message.ts
@@ -4,28 +4,31 @@ import { v4 as uuidv4 } from 'uuid';
 
 const MESSAGES_LIMIT = 20;
 
-export const sendAnonymousMessage = (msg: Message, messagesInMemory: AnonymousMessage[], targetGuild: Guild, targetChannelName: string): void => {
-        const anonymousMessage = {
-          id: uuidv4(),
-          content: msg.content,
-          author: msg.author,
-        };
-    
-        if (messagesInMemory.length >= MESSAGES_LIMIT) {
-            messagesInMemory.pop();
-        }
-        messagesInMemory.unshift(anonymousMessage);
-    
-        const givenChannel = targetGuild.channels.cache.find(
-          (channel) => channel.name === targetChannelName
-        ) as TextChannel
-    
-        if (givenChannel) {
-          givenChannel.send(
-            `Wiadomość nr: ${anonymousMessage.id}\n${msg.content}`
-          );
-          msg.reply('Anonimowa wiadomosć została dodana');
-        } else {
-          throw new Error('Text Channel not found. Check bot config!');
-        }
-}
+export const sendAnonymousMessage = (
+  msg: Message,
+  messagesInMemory: AnonymousMessage[],
+  targetGuild: Guild,
+  targetChannelName: string
+): void => {
+  const anonymousMessage = {
+    id: uuidv4(),
+    content: msg.content,
+    author: msg.author,
+  };
+
+  if (messagesInMemory.length >= MESSAGES_LIMIT) {
+    messagesInMemory.pop();
+  }
+  messagesInMemory.unshift(anonymousMessage);
+
+  const givenChannel = targetGuild.channels.cache.find(
+    (channel) => channel.name === targetChannelName
+  ) as TextChannel;
+
+  if (givenChannel) {
+    givenChannel.send(`Wiadomość nr: ${anonymousMessage.id}\n${msg.content}`);
+    msg.reply('Anonimowa wiadomosć została dodana');
+  } else {
+    throw new Error('Text Channel not found. Check bot config!');
+  }
+};
